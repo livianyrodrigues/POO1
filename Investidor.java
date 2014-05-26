1b71cd6d041f6e64d7ea423395a8c7b380bb425e
Liviany Reis <livianyrodrigues@hotmail.com>
2014-05-26 11:28:02 -0300

    Criando uma classe cliente e uma classe investidor que é subclasse de cliente

---
+++ Investidor.java
@@ -0,0 +1,15 @@
+
+public class Investidor extends Cliente {
+	
+	private String tipo;
+	
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
+	
+
+}
